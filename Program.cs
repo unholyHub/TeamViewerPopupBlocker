8ba258c729fd3237b5568b34c3c1647e1851f442
Zhivko Kabaivanov <jivko89@yahoo.com>
2015-05-26 11:35:09 +0300

    Team Viewer Popup  Blocker
    The TeamViewer Popup Blocker is C# .NET Application using .NET 4
    Framework

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TeamViewerPopupBlocker
+{
+    public static class Program
+    {
+        private static Mutex mutex;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            bool result;
+            mutex = new Mutex(false, "5cb4795f-203f-4db7-bd9b-133c6d0565b1", out result);
+
+            if (!result)
+            {
+                MessageBox.Show("Another instance is already running.");
+                return;
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+
+            GC.KeepAlive(mutex);
+        }
+    }
+}
